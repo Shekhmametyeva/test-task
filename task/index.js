604c50cec1db126b19c6a6ba83faafd4ad338de2
Shekhmametyeva <alina-r-s98@outlook.com>
2022-07-03 11:14:02 +0400

    feat: add element rendering functionality

--- task/index.js
+++ task/index.js
@@ -1,6 +1,35 @@
 'use strict'
 
+
+
+
 const accordion = document.querySelector('.accordion');
+
+
+// draw accordion elements
+
+window.addEventListener('load', drawElements);
+
+function drawElements () {
+    for (let i in obj) {
+        const item = 
+        `<div class="accordion__item">
+            <div class="accordion__header">
+                <h2 class="accordion__title">${obj[i].title}</h2>
+                <img class="accordion__button" src="assets/icon/button.png" alt="button">
+            </div>
+            <div class="spoiler">
+                <img class="spoiler__img" src="assets/images/${obj[i].src}.jpg" alt="Монако">
+                <h4 class="spoiler__title">${obj[i].size}</h4> 
+                <p class="spoiler__text">${obj[i].text1}</p>
+                <p class="spoiler__text">${obj[i].text2}</p>
+            </div>
+        </div>`;
+        accordion.innerHTML += item;
+    }    
+}
+
+// open accordion
 
 accordion.addEventListener('click', (event) => {
     if (event.target.closest('.accordion__header')) {
