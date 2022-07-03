7749cd1899c67721a6deab4e0f6989364908daac
Shekhmametyeva <alina-r-s98@outlook.com>
2022-07-03 18:37:34 +0400

    refactor: change structure of the project

--- task/index.js
+++ task/index.js
@@ -1,46 +1,62 @@
 'use strict'
 
 const accordion = document.querySelector('.accordion');
-let size = getSize(document.documentElement.clientWidth);
 const popup = document.querySelector('.popup');
 const body = document.querySelector('.body');
+let size = getSize(document.documentElement.clientWidth);
 let isPopup = false;
+
+
+
+window.addEventListener('load', () => {
+    drawElements(accordion);
+
+    accordion.addEventListener('click', (event) => {
+        if (event.target.closest('.accordion__header')) {
+            const item = event.target.closest('.accordion__item');
+            toggleSpoiler(item, item.classList.contains('accordion__item_open'));
+        }
+        //popup
+        if (event.target.closest('.spoiler__img') && !isPopup || isPopup) {
+            togglePopup (event.target.closest('.spoiler__img'))
+        }
+    });  
+    
+    popup.addEventListener('click', togglePopup)
+});
+
+window.addEventListener('resize', () => {
+    const windowSize = getSize(document.documentElement.clientWidth);
+    if (size !== windowSize) {
+        console.log('draw')
+        accordion.innerHTML = ''; 
+        drawElements(accordion)
+    };
+    size = windowSize;
+}) 
 
 // draw accordion elements
 
-window.addEventListener('load', drawElements);
-
-function drawElements () {
-    for (let i in obj) {
+function drawElements (accordion) {
+    countries.forEach(el => {
         const item = 
         `<div class="accordion__item">
             <div class="accordion__header">
-                <h2 class="accordion__title">${obj[i].title}</h2>
+                <h2 class="accordion__title">${el.title}</h2>
                 <img class="accordion__button" src="assets/icon/button.png" alt="button">
             </div>
             <div class="spoiler">
-                <img class="spoiler__img" src="assets/images/${obj[i].src}.jpg" alt="Монако">
-                <h4 class="spoiler__title">${obj[i].size}</h4> 
-                <p class="spoiler__text">${obj[i].text1}</p>
-                <p class="spoiler__text">${obj[i].text2}</p>
+                <img class="spoiler__img" src="assets/images/${el.src}.jpg" alt="${el.title}">
+                <h4 class="spoiler__title">${el.size}</h4> 
+                <p class="spoiler__text">${el.text1}</p>
+                <p class="spoiler__text">${el.text2}</p>
             </div>
         </div>`;
         accordion.innerHTML += item;
-    }    
+    })     
 }
 
 // open accordion
-
-accordion.addEventListener('click', (event) => {
-    if (event.target.closest('.accordion__header')) {
-        const item = event.target.closest('.accordion__item');
-        toggleSpoiler(item, item.classList.contains('accordion__item_open'));
-    }
-    //popup
-    if (event.target.closest('.spoiler__img') && !isPopup || isPopup) {
-        togglePopup (event.target.closest('.spoiler__img'))
-    }
-});
 
 function toggleSpoiler (item , isOpen) {
     item.classList.toggle('accordion__item_open');  
@@ -61,33 +77,27 @@ function toggleSpoiler (item , isOpen) {
 
 // redraw elements when changing the screen width, for correct display
 
-window.addEventListener('resize', () => {
-    const windowSize = getSize(document.documentElement.clientWidth);
-    if (size !== windowSize) {
-        console.log('draw')
-        accordion.innerHTML = ''; 
-        drawElements()
-    };
-    size = windowSize;
-}) 
-
 function getSize (el) {
-  if (el <= 375) {
-    return 375
-  } else if (el > 375 && el <= 768) {
-    return 768
-  } else if (el > 768 && el <= 1280) {
-    return 1280
-  } else if (el > 1280 && el <= 1440) {
-    return 1440
-  } else if (el > 1440) {
-    return 1920
-  }
+    const SmallSizePx = 375;
+    const MediumSizePx = 768;
+    const LargeSizePx = 1280;
+    const ExtraLargeSizePx = 1440;
+    const ExtraExtraLargeSizePx = 1920;
+
+    if (el <= SmallSizePx) {
+        return SmallSizePx
+    } else if (el > SmallSizePx && el <= MediumSizePx) {
+        return MediumSizePx
+    } else if (el > MediumSizePx && el <= LargeSizePx) {
+        return LargeSizePx
+    } else if (el > LargeSizePx && el <= ExtraLargeSizePx) {
+        return ExtraLargeSizePx
+    } else if (el > ExtraLargeSizePx) {
+        return ExtraExtraLargeSizePx
+    }
 }
 
 // popup
-
-popup.addEventListener('click', togglePopup)
 
 function togglePopup (img) {
     if (!isPopup) {
